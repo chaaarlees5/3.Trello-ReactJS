4ec75a424ae0e353e90ed138038a5932d952a006
Charlô <charlesoliveira0293@hotmail.com>
2021-06-11 19:49:57 -0300

    v2 - feat: criado componente para adicionar o botão que cria cartões dentro das listas.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class AddCartao extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cartoes: []
+    }
+  }
+
+  criaCartao = () => {
+    const {cartoes} = this.state
+    cartoes.push({
+      id: cartoes.length,
+      card: 'Clique para alterar.',
+    })
+    this.setState({ cartoes });
+  }
+
+  render(){
+    const {cartoes} = this.state
+  
+    return (
+      <div>
+
+        {cartoes.map(itemC => (
+          <div key={itemC.id} className='cartao'>
+            <span contentEditable="true" suppressContentEditableWarning='true'>{itemC.card}</span>
+          </div>
+        ))}
+
+        <button onClick={this.criaCartao}>
+          Adicionar Cartão
+        </button>
+      
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      listas: [
-      ]
+      listas: []
     }
   }
+
 //Ao apertar botão, adiciona ao array 'lista' um elemento objeto com id, título e corpo.
   criaLista = () => {
     const {listas} = this.state
     listas.push({
-      listas, id: listas.length, 
-      titulo: `Lista ${listas.length+1} - Clique para alterar`, 
-      corpo: 'Digite o texto aqui',
+      listas, 
+        id: listas.length, 
+        titulo: `Lista ${listas.length+1} - Clique para alterar`,
+        botao: <AddCartao />
     })
     this.setState({ listas });
-  }
-
-  criaCartao = () => {
-    const {listas} = this.state
-  
   }
 
   render() {
     const {listas} = this.state
     return (
       <div className='colunas'>
-        {listas.map(item => (
-          <div key={item.id} className='caixa'>
-            <h3 contentEditable="true" suppressContentEditableWarning='true'>{item.titulo}</h3>
-            <span contentEditable="true" suppressContentEditableWarning='true'>{item.corpo}</span>
-            <br></br>
-            <button id={item.id} className='btnNovaLista' onClick={this.criaCartao}>Adicionar Cartão</button>
+        
+        {listas.map(itemL => (
+          <div key={itemL.id} className='lista'>
+            <h3 contentEditable="true" suppressContentEditableWarning='true'>{itemL.titulo}</h3>
+            <div>{itemL.botao}</div>
           </div>
         ))}
-          
+        
         <button onClick={this.criaLista} className='btnNovaLista'>
           Criar Lista
         </button>
